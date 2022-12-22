4172c2c47f34e20a8dda509b152a2e6fe1b24211
ayan471 <pakhiraayan@gmail.com>
2022-12-22 19:44:34 +0530

    styled show page

--- src/components/show/Details.js
+++ src/components/show/Details.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Details = ({ status, premiered, network }) => {
   return (
-    <Details>
+    <div>
       <p>
         Status: <span>{status}</span>
       </p>
       <p>
         Premiered {premiered} {network ? `on ${network.name}` : null}
       </p>
-    </Details>
+    </div>
   );
 };
 
