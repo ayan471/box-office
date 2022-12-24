84e96227e043df7da75226771ef1a40d029efff3
ayan471 <pakhiraayan@gmail.com>
2022-12-24 13:22:00 +0530

    defined theme for styled components and created a few styled components

--- src/App.js
+++ src/App.js
@@ -3,26 +3,37 @@ import { Switch, Route } from 'react-router';
 import Home from './pages/Home';
 import Show from './pages/Show';
 import Starred from './pages/Starred';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
+    <ThemeProvider theme={theme}>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
 
-      <Route exact path="/starred">
-        <Starred />
-      </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
 
-      <Route exact path="/show/:id">
-        <Show />
-      </Route>
+        <Route exact path="/show/:id">
+          <Show />
+        </Route>
 
-      <Route>
-        <div>Not Found</div>
-      </Route>
-    </Switch>
+        <Route>
+          <div>Not Found</div>
+        </Route>
+      </Switch>
+    </ThemeProvider>
   );
 }
 
